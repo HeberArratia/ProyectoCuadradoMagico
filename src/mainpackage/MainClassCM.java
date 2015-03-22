475991f2f50733806926a2cbfe605aa392ab16e3
Heber <heber.im87@gmail.com>
2015-03-21 21:56:39 -0300

    Código general

---
+++ src/mainpackage/MainClassCM.java
@@ -0,0 +1,16 @@
+package mainpackage;
+
+//@author Heber
+// Se omitieron tildes para evitar errores de lenguaje
+
+public class MainClassCM {
+
+    public static void main(String[] args) {
+       /* Instanciacion de la clase VistaCM enviando el
+        parametro true con el fin de hacerla visible.
+        */
+       VistaCM vista = new VistaCM();
+       vista.setVisible(true);
+    }
+    
+}
